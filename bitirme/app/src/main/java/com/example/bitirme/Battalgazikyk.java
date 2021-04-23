e885218295564e197caa60fb8e44b1602a03203b
hcemmm <hcemmm@gmail.com>
2021-04-23 18:24:20 +0300

    Eğitim/Yurt/Battalgazi

---
+++ bitirme/app/src/main/java/com/example/bitirme/Battalgazikyk.java
@@ -0,0 +1,254 @@
+package com.example.bitirme;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Battalgazikyk extends AppCompatActivity {
+
+    TextView yorumdaki_ad;
+    ImageView yorumyap_profilresmi;
+    EditText yorum_alani;
+    Button yorum_gonder,adresegit,battalwebegit;
+    String yorum,url,name_result,uid;
+    RecyclerView recyclerView;
+    FirebaseDatabase firebaseDatabase;
+    DocumentReference reference;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    FirebaseFirestore firestore;
+    DatabaseReference databaseReference;
+    Yorumkullanicilar yorumkullanicilar;
+    Tumkullanicilar tumkullanicilar;
+    DatabaseReference db1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battalgazikyk);
+
+        yorumdaki_ad = findViewById(R.id.yorumdaki_adsoyad);
+        yorum_alani = findViewById(R.id.battalgaziyorum_alani);
+        yorum_gonder = findViewById(R.id.battalgaziyorum_yap);
+        yorumyap_profilresmi = findViewById(R.id.battalgazikullanici_yorumpp);
+        yorumkullanicilar = new Yorumkullanicilar();
+        tumkullanicilar = new Tumkullanicilar();
+        recyclerView = findViewById(R.id.recycler_battalgaziyorumgor);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adresegit = findViewById(R.id.battalgaziharitagit);
+        battalwebegit = findViewById(R.id.battalgaziwebgit);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String kullan_id = user.getUid();
+
+        firestore = FirebaseFirestore.getInstance();
+        reference = firestore.collection("kullanici").document(kullan_id);
+
+        db1 = database.getReference("TumYorumlar").child("BattalgaziKYK").child(kullan_id);
+        databaseReference =database.getReference("BattalgaziKYK").child(kullan_id);
+
+
+        battalwebegit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent webintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kygm.gsb.gov.tr/YurtMudurlukleri"));
+                startActivity(webintent);
+
+            }
+        });
+
+        adresegit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent haritaintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/dir//Bulgurlu,+BATTALGAZ%C4%B0+KIZ+%C3%96%C4%9ERENC%C4%B0+YURDU+VE+BLOKLARI,+kanyon+caddesi+no:39%2F5,+44100+Battalgazi%2FMalatya/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x40763723171f5243:0xbc6766d1930c4a43?sa=X&ved=2ahUKEwiFjbGdhIPwAhWRwQIHHTVTDBsQ48ADMAB6BAgEEA8"));
+                startActivity(haritaintent);
+            }
+        });
+        yorum_gonder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                yorum_yap();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        reference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.getResult().exists()) {
+                            name_result = task.getResult().getString("ad");
+                            uid = task.getResult().getString("uid");
+
+                            url = task.getResult().getString("url");
+                            Picasso.get().load(url).into(yorumyap_profilresmi);
+
+                        }
+                    }
+                });
+
+        FirebaseRecyclerOptions<Yorumkullanicilar> options =
+                new FirebaseRecyclerOptions.Builder<Yorumkullanicilar>()
+                        .setQuery(databaseReference, Yorumkullanicilar.class)
+                        .build();
+
+
+        FirebaseRecyclerAdapter<Yorumkullanicilar, Yorumsahip> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Yorumkullanicilar, Yorumsahip>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull Yorumsahip holder, int position, @NonNull Yorumkullanicilar model) {
+                        holder.setData(getApplicationContext(), model.getYorum(),model.getAdsoyad(),model.getUrl(),model.getUid(),model.getZaman());
+
+
+                        holder.setProfile(getApplicationContext());
+                        String guid = getItem(position).getUid();
+
+
+                        holder.yorumprofilbak.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                Intent intent = new Intent(getApplicationContext(),GosterkullaniciActivity.class);
+                                intent.putExtra("guid",guid);
+                                startActivity(intent);
+                            }
+                        });
+
+                        holder.setOnClickListener(new Yorumsahip.Clicklistener() {
+                            @Override
+                            public void onItemlongClick(View view, int position) {
+
+                                yorum = getItem(position).getYorum();
+
+                                showDeleteDataDialog(yorum);
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public Yorumsahip onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        View view = LayoutInflater.from(parent.getContext())
+                                .inflate(R.layout.yorumcu_items, parent, false);
+
+
+                        return new Yorumsahip(view);
+                    }
+                };
+        firebaseRecyclerAdapter.startListening();
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    private void showDeleteDataDialog(String yorum) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Battalgazikyk.this);
+        builder.setTitle("Sil");
+        builder.setMessage("Yorumunuz Silinsin mi?");
+        builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+
+                Query query = databaseReference.orderByChild("yorum").equalTo(yorum);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            ds.getRef().removeValue();
+                        }
+                        Toast.makeText(Battalgazikyk.this, "Yorum Silindi", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+        builder.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void yorum_yap() {
+        Calendar callfordate = Calendar.getInstance();
+        SimpleDateFormat currentdate = new
+                SimpleDateFormat("dd-MMMM-yyyy");
+        final  String savedate = currentdate.format(callfordate.getTime());
+
+        Calendar callfortime = Calendar.getInstance();
+        SimpleDateFormat currenttime = new
+                SimpleDateFormat("HH:mm");
+        final  String savetime = currenttime.format(callfortime.getTime());
+
+        String time = savedate+":"+savetime;
+        String comment = yorum_alani.getText().toString();
+        if (comment != null){
+
+            yorumkullanicilar.setYorum(comment);
+            yorumkullanicilar.setAdsoyad(name_result);
+            yorumkullanicilar.setUid(uid);
+            yorumkullanicilar.setUrl(url);
+            yorumkullanicilar.setZaman(time);
+
+
+            String id = db1.push().getKey();
+            databaseReference.child(id).setValue(yorumkullanicilar);
+
+            yorum_alani.setText("");
+
+            Toast.makeText(this, "Yorum Yapıldı.", Toast.LENGTH_SHORT).show();
+
+        }else {
+            Toast.makeText(this, "Lütfen Yorumunuzu Yazınız...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
