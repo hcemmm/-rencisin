66574693cff1689ecb1f3b252f63e033f99f3719
hcemmm <hcemmm@gmail.com>
2021-04-27 14:56:10 +0300

    Eğitim/Yurt/Hazar

--- bitirme/app/src/main/java/com/example/bitirme/Hazaryurt.java
+++ bitirme/app/src/main/java/com/example/bitirme/Hazaryurt.java
@@ -54,7 +54,7 @@ public class Hazaryurt extends AppCompatActivity {
     DatabaseReference databaseReference;
     Yorumkullanicilar yorumkullanicilar;
     Tumkullanicilar tumkullanicilar;
-    DatabaseReference db1;
+    DatabaseReference db1,db2,db3;
 
 
     @Override
@@ -80,8 +80,12 @@ public class Hazaryurt extends AppCompatActivity {
         firestore = FirebaseFirestore.getInstance();
         reference = firestore.collection("kullanici").document(kullan_id);
 
-        db1 = database.getReference("TumYorumlar").child("HazarYurt").child(kullan_id);
-        databaseReference =database.getReference("HazarYurt").child(kullan_id);
+        db1 = database.getReference("Tumyorumlar").child("HazarYurt").child(kullan_id);
+        db2 = database.getReference("KYorumlar").child(kullan_id);
+        db3 = database.getReference("Yorumlar").child("Hazar");
+        db3.keepSynced(true);
+
+        databaseReference =database.getReference("Yorumlar").child("Hazar");
 
 
         hazarwebegit.setOnClickListener(new View.OnClickListener() {
@@ -139,11 +143,30 @@ public class Hazaryurt extends AppCompatActivity {
                 new FirebaseRecyclerAdapter<Yorumkullanicilar, Yorumsahip>(options) {
                     @Override
                     protected void onBindViewHolder(@NonNull Yorumsahip holder, int position, @NonNull Yorumkullanicilar model) {
-                        holder.setData(getApplicationContext(), model.getYorum(),model.getAdsoyad(),model.getUrl(),model.getUid(),model.getZaman());
+                        holder.setData(getApplicationContext(), model.getYorum(),model.getAdsoyad(),model.getUrl(),model.getUid(),model.getZaman(),model.getKonum());
 
 
                         holder.setProfile(getApplicationContext());
                         String guid = getItem(position).getUid();
+
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        String kullans_id = user.getUid();
+
+
+                        yorum = getItem(position).getYorum();
+
+                        holder.yorumdasil.setOnClickListener(new View.OnClickListener() {
+
+                            @Override
+                            public void onClick(View v) {
+                                if (guid.equals(kullans_id)){
+                                    holder.yorumdasil.setVisibility(View.VISIBLE);
+                                    yorumsil(yorum);
+                                }else {
+                                    holder.yorumdasil.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        });
 
 
                         holder.yorumprofilbak.setOnClickListener(new View.OnClickListener() {
@@ -156,15 +179,6 @@ public class Hazaryurt extends AppCompatActivity {
                             }
                         });
 
-                        holder.setOnClickListener(new Yorumsahip.Clicklistener() {
-                            @Override
-                            public void onItemlongClick(View view, int position) {
-
-                                yorum = getItem(position).getYorum();
-
-                                showDeleteDataDialog(yorum);
-                            }
-                        });
                     }
 
                     @NonNull
@@ -182,14 +196,13 @@ public class Hazaryurt extends AppCompatActivity {
         recyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 
-    private void showDeleteDataDialog(String yorum) {
+    private void yorumsil(String yorum) {
         AlertDialog.Builder builder = new AlertDialog.Builder(Hazaryurt.this);
         builder.setTitle("Sil");
         builder.setMessage("Yorumunuz Silinsin mi?");
         builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
-
                 Query query = databaseReference.orderByChild("yorum").equalTo(yorum);
                 query.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
@@ -204,6 +217,24 @@ public class Hazaryurt extends AppCompatActivity {
                     public void onCancelled(@NonNull DatabaseError databaseError) {
 
                     }
+
+                });
+
+                Query query2 = db2.orderByChild("yorum").equalTo(yorum);
+                query2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            ds.getRef().removeValue();
+                        }
+                        Toast.makeText(Hazaryurt.this, "Yorum Silindi", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+
                 });
             }
         });
@@ -215,8 +246,9 @@ public class Hazaryurt extends AppCompatActivity {
         });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
-    }
 
+
+    }
     private void yorum_yap() {
         Calendar callfordate = Calendar.getInstance();
         SimpleDateFormat currentdate = new
@@ -237,10 +269,17 @@ public class Hazaryurt extends AppCompatActivity {
             yorumkullanicilar.setUid(uid);
             yorumkullanicilar.setUrl(url);
             yorumkullanicilar.setZaman(time);
+            yorumkullanicilar.setKonum("Hazar Kız Yurdu");
 
 
             String id = db1.push().getKey();
-            databaseReference.child(id).setValue(yorumkullanicilar);
+            db1.child(id).setValue(yorumkullanicilar);
+
+            String id2 = db2.push().getKey();
+            db2.child(id2).setValue(yorumkullanicilar);
+
+            String id1 = db3.push().getKey();
+            db3.child(id1).setValue(yorumkullanicilar);
 
             yorum_alani.setText("");
 
