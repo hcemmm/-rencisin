e57085715cd03b38928179e63e860dd4afed0a60
hcemmm <hcemmm@gmail.com>
2021-04-27 14:56:25 +0300

    Eğitim/Üniversite/İnönü

--- bitirme/app/src/main/java/com/example/bitirme/Inonuuniversite.java
+++ bitirme/app/src/main/java/com/example/bitirme/Inonuuniversite.java
@@ -145,13 +145,29 @@ public class Inonuuniversite extends AppCompatActivity {
                 new FirebaseRecyclerAdapter<Yorumkullanicilar, Yorumsahip>(options) {
                     @Override
                     protected void onBindViewHolder(@NonNull Yorumsahip holder, int position, @NonNull Yorumkullanicilar model) {
-                        holder.setData(getApplicationContext(), model.getYorum(),model.getAdsoyad(),model.getUrl(),model.getUid(),model.getZaman());
-
+                        holder.setData(getApplicationContext(), model.getYorum(),model.getAdsoyad(),model.getUrl(),model.getUid(),model.getZaman(),model.getKonum());
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        String kullans_id = user.getUid();
 
                         holder.setProfile(getApplicationContext());
                         String guid = getItem(position).getUid();
 
+                        yorum = getItem(position).getYorum();
 
+
+                        holder.yorumdasil.setOnClickListener(new View.OnClickListener() {
+
+                            @Override
+                            public void onClick(View v) {
+                                if (guid.equals(kullans_id)){
+                                    holder.yorumdasil.setVisibility(View.VISIBLE);
+                                    yorumsil(yorum);
+                                }else {
+                                    holder.yorumdasil.setVisibility(View.INVISIBLE);
+                                }
+
+                            }
+                        });
                         holder.yorumprofilbak.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -162,15 +178,6 @@ public class Inonuuniversite extends AppCompatActivity {
                             }
                         });
 
-                        holder.setOnClickListener(new Yorumsahip.Clicklistener() {
-                            @Override
-                            public void onItemlongClick(View view, int position) {
-
-                                yorum = getItem(position).getYorum();
-
-                                 showDeleteDataDialog(yorum);
-                            }
-                        });
                     }
 
                     @NonNull
@@ -188,16 +195,32 @@ public class Inonuuniversite extends AppCompatActivity {
         recyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 
-   private void showDeleteDataDialog(String yorum) {
+    private void yorumsil(String yorum) {
         AlertDialog.Builder builder = new AlertDialog.Builder(Inonuuniversite.this);
         builder.setTitle("Sil");
         builder.setMessage("Yorumunuz Silinsin mi?");
         builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
-
                 Query query = databaseReference.orderByChild("yorum").equalTo(yorum);
                 query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            ds.getRef().removeValue();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+
+                });
+
+                Query query2 = db2.orderByChild("yorum").equalTo(yorum);
+                query2.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         for (DataSnapshot ds : dataSnapshot.getChildren()) {
@@ -210,6 +233,7 @@ public class Inonuuniversite extends AppCompatActivity {
                     public void onCancelled(@NonNull DatabaseError databaseError) {
 
                     }
+
                 });
             }
         });
@@ -221,7 +245,10 @@ public class Inonuuniversite extends AppCompatActivity {
         });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+
+
     }
+
 
     private void yorum_yap() {
         Calendar callfordate = Calendar.getInstance();
@@ -243,6 +270,7 @@ public class Inonuuniversite extends AppCompatActivity {
             yorumkullanicilar.setUid(uid);
             yorumkullanicilar.setUrl(url);
             yorumkullanicilar.setZaman(time);
+            yorumkullanicilar.setKonum("İnönü Üniversitesi");
 
 
 
