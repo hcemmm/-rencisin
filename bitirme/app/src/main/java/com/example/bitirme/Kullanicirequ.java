d018d632f0be5b731fbdebd21505569eed0326ab
hcemmm <hcemmm@gmail.com>
2021-05-06 17:01:13 +0300

    Create Kullanicirequ.java

---
+++ bitirme/app/src/main/java/com/example/bitirme/Kullanicirequ.java
@@ -0,0 +1,56 @@
+package com.example.bitirme;
+
+public class Kullanicirequ {
+    String gosteradsoy,url,gosteryas,gosteriletisim;
+    String privacy;
+
+    public String getGosteradsoy() {
+        return gosteradsoy;
+    }
+
+    public void setGosteradsoy(String gosteradsoy) {
+        this.gosteradsoy = gosteradsoy;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getGosteryas() {
+        return gosteryas;
+    }
+
+    public void setGosteryas(String gosteryas) {
+        this.gosteryas = gosteryas;
+    }
+
+    public String getGosteriletisim() {
+        return gosteriletisim;
+    }
+
+    public void setGosteriletisim(String gosteriletisim) {
+        this.gosteriletisim = gosteriletisim;
+    }
+
+    public String getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(String privacy) {
+        this.privacy = privacy;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    String userid;
+}
