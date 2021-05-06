b9d8a6a0f8394bd6bfe17328531a51e6c5d56cba
hcemmm <hcemmm@gmail.com>
2021-05-06 17:00:51 +0300

    Create Gonderikullanicilar.java

---
+++ bitirme/app/src/main/java/com/example/bitirme/Gonderikullanicilar.java
@@ -0,0 +1,65 @@
+package com.example.bitirme;
+
+public class Gonderikullanicilar {
+    String gkAdsoyad, gkUrl, gkPostUri, gkTime, gkUid, gkType, gkAciklama;
+
+    public Gonderikullanicilar(){
+
+    }
+
+    public String getGkAdsoyad() {
+        return gkAdsoyad;
+    }
+
+    public void setGkAdsoyad(String gkAdsoyad) {
+        this.gkAdsoyad = gkAdsoyad;
+    }
+
+    public String getGkUrl() {
+        return gkUrl;
+    }
+
+    public void setGkUrl(String gkUrl) {
+        this.gkUrl = gkUrl;
+    }
+
+    public String getGkPostUri() {
+        return gkPostUri;
+    }
+
+    public void setGkPostUri(String gkPostUri) {
+        this.gkPostUri = gkPostUri;
+    }
+
+    public String getGkTime() {
+        return gkTime;
+    }
+
+    public void setGkTime(String gkTime) {
+        this.gkTime = gkTime;
+    }
+
+    public String getGkUid() {
+        return gkUid;
+    }
+
+    public void setGkUid(String gkUid) {
+        this.gkUid = gkUid;
+    }
+
+    public String getGkType() {
+        return gkType;
+    }
+
+    public void setGkType(String gkType) {
+        this.gkType = gkType;
+    }
+
+    public String getGkAciklama() {
+        return gkAciklama;
+    }
+
+    public void setGkAciklama(String gkAciklama) {
+        this.gkAciklama = gkAciklama;
+    }
+}
