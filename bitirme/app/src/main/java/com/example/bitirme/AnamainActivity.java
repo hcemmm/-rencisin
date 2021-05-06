7848e4228ea83a62826f434fce10b651c91fea20
hcemmm <hcemmm@gmail.com>
2021-05-06 16:56:52 +0300

    Create AnamainActivity.java

---
+++ bitirme/app/src/main/java/com/example/bitirme/AnamainActivity.java
@@ -0,0 +1,23 @@
+package com.example.bitirme;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import javax.annotation.Nullable;
+
+public class AnamainActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_anamain, container, false);
+        return view;
+
+    }
+}
