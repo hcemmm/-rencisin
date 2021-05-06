3a6fb56d58569e7e9d2a0a440c579e3a26f1e904
hcemmm <65783885+hcemmm@users.noreply.github.com>
2021-05-06 18:07:51 +0300

    Diğer Kullanıcı Profilleri (Showuser)

--- bitirme/app/src/main/java/com/example/bitirme/GosterkullaniciActivity.java
+++ bitirme/app/src/main/java/com/example/bitirme/GosterkullaniciActivity.java
@@ -30,7 +30,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
-
+ 
 public class GosterkullaniciActivity extends AppCompatActivity {
 
     TextView gosteradi,gosteryasi,gosteriletisim;
