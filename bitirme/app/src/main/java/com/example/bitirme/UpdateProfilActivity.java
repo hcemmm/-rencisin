a485613f35e18f7b9f10f5ee0145da5f01586193
hcemmm <hcemmm@gmail.com>
2021-05-06 17:01:29 +0300

    Update UpdateProfilActivity.java

--- bitirme/app/src/main/java/com/example/bitirme/UpdateProfilActivity.java
+++ bitirme/app/src/main/java/com/example/bitirme/UpdateProfilActivity.java
@@ -1,15 +1,15 @@
 package com.example.bitirme;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -17,8 +17,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -29,11 +27,13 @@ import com.squareup.picasso.Picasso;
 public class UpdateProfilActivity extends AppCompatActivity {
 
     EditText adsoyaddz,yasdz,iletisimdz;
-    Button button;
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference reference;
+    Button button,ayarlar;
     DocumentReference documentReference ;
+    ImageView new_foto;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String  kullanici_id;
+
+    private final static int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +42,32 @@ public class UpdateProfilActivity extends AppCompatActivity {
 
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String  kullanici_id= user.getUid();
+        kullanici_id= user.getUid();
 
         documentReference = db.collection("kullanici").document(kullanici_id);
 
+        new_foto = findViewById(R.id.nw_updatefoto);
         iletisimdz = findViewById(R.id.et_iletisim);
         adsoyaddz = findViewById(R.id.et_name_up);
         yasdz = findViewById(R.id.et_yas);
         button = findViewById(R.id.btn_up);
+        ayarlar=findViewById(R.id.gizlilik_ayar);
 
+        ayarlar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Hesapgizlilik.class));
+                finish();
+            }
+        });
+
+        new_foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ProfilFotoUpdate.class);
+                startActivity(intent);
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,8 +82,6 @@ public class UpdateProfilActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-
-
         documentReference.get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
@@ -75,10 +90,10 @@ public class UpdateProfilActivity extends AppCompatActivity {
                         if (task.getResult().exists()){
                             String adsoyadResult = task.getResult().getString("ad");
                             String iletisimResult = task.getResult().getString("iletisim");
-                            String url = task.getResult().getString("url");
                             String yasResult = task.getResult().getString("yas");
+                            String urlresult = task.getResult().getString("url");
 
-
+                            Picasso.get().load(urlresult).into(new_foto);
                             adsoyaddz.setText(adsoyadResult);
                             iletisimdz.setText(iletisimResult);
                             yasdz.setText(yasResult);
@@ -97,8 +112,6 @@ public class UpdateProfilActivity extends AppCompatActivity {
         final String contex = iletisimdz.getText().toString();
         final String age = yasdz.getText().toString();
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String kullanici_id= user.getUid();
         final  DocumentReference sDoc = db.collection("kullanici").document(kullanici_id);
         db.runTransaction(new Transaction.Function<Void>() {
             @Override
